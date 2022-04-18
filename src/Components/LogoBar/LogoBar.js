9f4a666f28ea88b39fd4d3ce12fc55b67ac12e39
chidera <chidera.diandra@gmail.com>
2022-04-18 14:41:47 +0100

    book a truck corrected

--- src/Components/LogoBar/LogoBar.js
+++ src/Components/LogoBar/LogoBar.js
@@ -2,22 +2,21 @@ import React from "react";
 import styles from "./LogoBar.module.css";
 import WebLogo from "../../Asset/Icons/webLogo.svg";
 import MobileLogo from "../../Asset/Icons/mobileLogo.svg";
+import { Link } from "react-router-dom";
 
 const LogoBar = () => {
-    return (
-        <div className={styles.logoBar}>
-            <div>
-                <picture>
-                    <source
-                        srcset = {MobileLogo}
-                        media ="(max-width: 40rem)"
-                    ></source>
-                    <img src={WebLogo} alt="Haulk's Logo"/>
-                </picture>
-            </div>
+  return (
+    <div className={styles.logoBar}>
+      <div>
+        <picture>
+          <source srcset={MobileLogo} media="(max-width: 40rem)"></source>
+          <Link to="/">
+            <img src={WebLogo} alt="Haulk's Logo" />
+          </Link>
+        </picture>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    );
-}
- 
 export default LogoBar;
