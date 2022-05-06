625a3b29d36d95c7da51388719c34fa73b299761
chidera <chidera.diandra@gmail.com>
2022-05-06 16:22:00 +0100

    unverified

--- src/Components/TruckDriver/OrderHistory.js
+++ src/Components/TruckDriver/OrderHistory.js
@@ -3,34 +3,47 @@ import Tables from "../Table/Tables";
 import style from "./OrderHistory.module.css";
 import { useSelector } from "react-redux";
 import EmptyTable from "../Table/EmptyTable"
+import padloc from "../../Asset/Images/Padloc.svg"
+import styles from "./body.module.css";
 
 const OrderHistory = () => {
-  const { orderHistory } = useSelector(state => state.truckDriverOrders);
+  const { orderHistory, profile } = useSelector(state => state.truckDriverOrders);
 
   return (
 
 
     <div className={style.OrderHistory__container}>
-      {
-        (orderHistory && orderHistory.length !== 0) ?
-          <Tables
-            title="Order History"
-            header={[
-              "ID",
-              "Date Requested",
-              "Status",
-              "Truck Type",
-              "Drop Off",
-              "",
-            ]}
-            content={orderHistory && orderHistory}
-          /> : <div> <EmptyTable
-            title="Orders"
-            header="You've not made any orders yet"
-            content="Your orders will be displayed here."
+      {profile && profile.accepted === "verified" ?
+        <div>
+          {
+            (orderHistory && orderHistory.length !== 0) ?
+              <Tables
+                title="Order History"
+                header={[
+                  "ID",
+                  "Date Requested",
+                  "Status",
+                  "Truck Type",
+                  "Drop Off",
+                  "",
+                ]}
+                content={orderHistory && orderHistory}
+              /> : <div> <EmptyTable
+                title="Orders"
+                header="You've not made any orders yet"
+                content="Your orders will be displayed here."
 
-          /></div>
-      }
+              /></div>
+          }
+        </div> : <div className={styles.notVerified__container} style={{ width: "100%" }}>
+          <div className={styles.notverified__padlock}>
+            <img src={padloc} alt="Pad Lock" />
+          </div>
+          <div className={styles.notverified__content}>
+            <h5> Your account has not been verified </h5>
+            <p> Kindly wait while we verify your documents. Check back often.</p>
+          </div>
+        </div>}
     </div>
 
   );
