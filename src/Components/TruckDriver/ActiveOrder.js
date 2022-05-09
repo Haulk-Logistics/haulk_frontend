376cc5bc247c37fcb2d6a12e1785a6ef90a2332c
chidera <chidera.diandra@gmail.com>
2022-05-09 19:07:11 +0100

    truck driver css correction for order page

--- src/Components/TruckDriver/ActiveOrder.js
+++ src/Components/TruckDriver/ActiveOrder.js
@@ -21,39 +21,40 @@ const ActiveOrder = () => {
   }, [activeOrder]);
 
 
+  //
+
+
+
   return (
-    <div className={style.Active_OrderSection} >
+    <div>
+      {profile && profile.accepted === "verified" ?
+        <div className={style.Active_OrderSection} >
+          <div className={style.Active__OrderContainer}>
+            <header className={style.Active__header}>
+              <h5>Active Order</h5>
+            </header>
+            {
+              activeOrder && typeof activeOrder !== "string" ? <div onClick={() => { activeOrder && dispatch(OrderDetails(activeOrder)); dispatch(orderModalStatus({ status: true })) }}>
 
-      <div className={style.Active__OrderContainer}>
-        {profile && profile.accepted === "verified" ?
-          <div>
-            <div>
-              <header className={style.Active__header}>
-                <h5>Active Order</h5>
-              </header>
-              {
-                activeOrder && typeof activeOrder !== "string" ? <div onClick={() => { activeOrder && dispatch(OrderDetails(activeOrder)); dispatch(orderModalStatus({ status: true })) }}>
+                <TDActiveOrderCard activeOrder={activeOrder} />
 
-                  <TDActiveOrderCard activeOrder={activeOrder} />
-
-                </div> :
-                  <Active />
-              }
-              <Open />
-            </div>
-            <div className={style.OrderDetails}>
-              <OrderDetail />
-            </div>
-          </div> : <div className={styles.notVerified__container}>
-            <div className={styles.notverified__padlock}>
-              <img src={padloc} alt="Pad Lock" />
-            </div>
-            <div className={styles.notverified__content}>
-              <h5> Your account has not been verified </h5>
-              <p> Kindly wait while we verify your documents. Check back often.</p>
-            </div>
-          </div>}
-      </div>
+              </div> :
+                <Active />
+            }
+            <Open />
+          </div>
+          <div className={style.OrderDetails}>
+            <OrderDetail />
+          </div>
+        </div> : <div className={style.notVerified__container}>
+          <div className={style.notverified__padlock}>
+            <img src={padloc} alt="Pad Lock" />
+          </div>
+          <div className={styles.notverified__content}>
+            <h5> Your account has not been verified </h5>
+            <p> Kindly wait while we verify your documents. Check back often.</p>
+          </div>
+        </div>}
     </div>
   );
 };
